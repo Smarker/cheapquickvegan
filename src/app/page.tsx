b105d1a37995fe57f4ed5e2fbe11ec2e3e508dcf
Stephanie Marker <stephanie.marker93@gmail.com>
2025-10-26 15:14:18 -0400

    add vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { getPostsFromCache, Post } from "@/lib/notion";
 import PostCard from "@/components/post-card";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   const posts = getPostsFromCache();
 
   return (
     <div>
+      <Analytics />
       <div className="max-w-2xl mx-auto text-center mb-12">
         <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl mb-4">
           Welcome to CheapQuickVegan
