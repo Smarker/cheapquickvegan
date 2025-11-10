a10d9e9a8694753042cec559cbec6fb147062799
Stephanie Marker <stephanie.marker93@gmail.com>
2025-11-10 14:20:37 -0500

    stripe buy button

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,14 @@
-// src/types/global.d.ts
-declare namespace JSX {
-  interface IntrinsicElements {
-    'stripe-buy-button': {
-      'buy-button-id': string;
-      'publishable-key': string;
-      [key: string]: any;
-    };
+// src/types/stripe-buy-button.d.ts
+import 'react';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'stripe-buy-button': {
+        'buy-button-id': string;
+        'publishable-key': string;
+        [key: string]: any; // optional, allow other props like className
+      };
+    }
   }
 }
