bbeab528f7016c79fb225ebece6a43e05546a409
Stephanie Marker <stephanie.marker93@gmail.com>
2025-11-19 21:06:17 -0500

    add bfast taco recipe

---
+++ src/app/guides/vegan-ground-beef-tips/page.tsx
@@ -0,0 +1,8 @@
+beyond burgers are better
+look the same the whole time
+don't stick to the bottom of the pan if using a cast iron
+
+impossible better for ground Beef
+can tell when theyre done; change from red to brown - more similar to actual meat
+better for tacos
+stick to bottom of the pan if using a cast iron
