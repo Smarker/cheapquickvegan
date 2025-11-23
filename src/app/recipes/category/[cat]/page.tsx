2b1ecef020b21d370fdd38b749af25459a64a283
Stephanie Marker <stephanie.marker93@gmail.com>
2025-11-23 18:53:01 -0500

    fix bug

--- src/app/recipes/category/[cat]/page.tsx
+++ src/app/recipes/category/[cat]/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface CategoryPageProps {
-  params: { cat: string };
+  params: Promise<{ cat: string }>;
 }
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
