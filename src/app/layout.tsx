210980b157b4d41f87fff08bd2c9d1f9dfdec5dc
Stephanie Marker <stephanie.marker93@gmail.com>
2025-11-29 23:21:24 -0500

    try once more gdpr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   alternates: {
     canonical: siteUrl
   },
-  manifest: "/site.webmanifest",
+//  manifest: "/site.webmanifest",
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
