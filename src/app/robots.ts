bb9c5d0f41a804218386ace8245d2dbb32b76ce2
Stephanie Marker <stephanie.marker93@gmail.com>
2025-11-30 00:07:22 -0500

    try this

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -14,7 +14,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: [
         "/private/",
         "/_next/data/", // Next.js data JSON files
-        "/site.webmanifest", // not meant for indexing
         "/*.ico$", // favicon or icon files
       ]
     },
